ff69216578378425f54dc2230a2ee90c6513cbad
moksha <mokshagnaswarna58@gmail.com>
2026-06-27 09:02:09 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements(file_path: str) -> list[str]:
+    """This function will return the list of requirements"""
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+setup(
+    name="ml project2",
+    version="0.0.1",
+    author="mokshagna",
+    author_email="<mokshagnaswarn@gmail.com>",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
+        
